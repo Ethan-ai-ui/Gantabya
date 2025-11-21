91fa050aa398c1ea801bceff3c4fcfe50032a75e
pp-beb <prithakkoirala44@gmail.com>
2025-11-21 12:16:09 +0545

    fooookin hell

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,19 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+ origin: function(origin, callback) {
+  // Allow no-origin requests (like mobile apps, curl, Postman, Vite preflight)
+  if (!origin) {
+    return callback(null, true);
+  }
+
+  if (allowedOrigins.includes(origin)) {
+    return callback(null, true);
+  }
+
+  return callback(new Error('Not allowed by CORS'));
+},
+
   credentials: true
 }));
 
