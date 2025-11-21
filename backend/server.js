7cb6d4fa82bdb3888cacefda3cfeb75c77a60b69
pp-beb <prithakkoirala44@gmail.com>
2025-11-21 12:31:25 +0545

    kill me

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ const allowedOrigins = [
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self' https://fonts.gstatic.com;"
+  );
+  next();
+});
+
+
 app.use(bodyParser.json());
 
 const usersFilePath = path.join(__dirname, 'users.json');
